2a06a9dcf6d658247dfc4bd1db0368789eb93bb1
DawoodKhatri <dawoodkhatri18@gmail.com>
2023-02-21 20:38:43 +0530

    hide api key

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -5,7 +5,7 @@ const Map = ({ location, className, style }) => {
   const mapRef = useRef();
 
   const mapLoader = new Loader({
-    apiKey: "AIzaSyDYnsc3xs3i7xLMt30NFrl25Dbcx7_UXjU", //This is test api key
+    // apiKey: "AIzaSyDYnsc3xs3i7xLMt30NFrl25Dbcx7_UXjU", //This is test api key
     libraries: ["places"],
   });
   console.log(location);
